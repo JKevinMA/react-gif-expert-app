393060b8560bcea5347f62a5b0694e3fbcdb4105
kmamani <isa39@unibanca.com.pe>
2022-09-11 23:33:25 -0500

    first commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,13 @@
+export const getGifs = async (category) => {
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=2q0jUsd4obeRz4BUBQgb73K7fGHA7Y06`;
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+    const gifs = data.map(img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    });
+    return gifs;
+}
